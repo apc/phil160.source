e1e6ba9194936eb5bb71a689daaeae3192bca80b
A.P.C <apc@umass.edu>
2014-01-23 20:29:53 -0500

    Changed theme, added a few things.

--- config.rb
+++ config.rb
@@ -4,11 +4,11 @@ require 'sass-globbing'
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
+http_path = "/phil160/"
+http_images_path = "/phil160/images"
 http_generated_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_fonts_path = "/phil160/fonts"
+css_dir = "public/phil160/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
